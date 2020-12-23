0d11f8a58d602fa37fea3815842d33b3abb631c3
Jonathanplas <jplas12345@gmail.com>
2020-12-22 20:25:13 -0500

    major breakthrough and bug fix

--- src/app/common/services/phenoType.model.js
+++ src/app/common/services/phenoType.model.js
@@ -1,59 +1,61 @@
 // Phenotype model definition, used for entries in the phenotable
 
-class PhenotypeModel {    
-    constructor(Parse) {        
-        this.Parse = Parse;        
-        this.data = {};        
-        this.collection = [];        
-        this.name = 'Phenotype';        
-        this.fields = [                       
-            'position',            
-            'name',            
-            'highest_pos',            
-            'region1',            
-            'region2',
-            'region3'        
-        ];    
-    } 
-    // New method   
-    New(obj) {        
-        if (angular.isUndefined(obj)) {            
-            const parseObject = new this.Parse.Object(this.name)            
-            this.Parse.defineAttributes(parseObject, this.fields);            
-            return parseObject;        
-        } else {            
-            this.Parse.defineAttributes(obj, this.fields);            
-            return obj;        
-        }    
-    }   
-    // Retrieve by ID 
-    getById(id) {        
-        return new this.Parse.Query(this.New())            
-        .get(id)            
-        .then(result => {                
-            this.Parse.defineAttributes(result, this.fields);                
-            this.data = result;                
-            return Promise.resolve(result);            
-        })            
-        .catch(error => Promise.reject(error));    
-    }    
+class PhenotypeModel {
+    constructor(Parse) {
+        this.Parse = Parse;
+        this.data = {};
+        this.collection = [];
+        this.name = 'Phenotype';
+        this.fields = [
+          'nameChinese',
+          'namePinyin',
+          'originalRegion',
+          'source',
+          'gender',
+          'dynasty',
+          'emperor',
+          'highestPosition'
+        ];
+    }
+    // New method
+    New(obj) {
+        if (angular.isUndefined(obj)) {
+            const parseObject = new this.Parse.Object(this.name)
+            this.Parse.defineAttributes(parseObject, this.fields);
+            return parseObject;
+        } else {
+            this.Parse.defineAttributes(obj, this.fields);
+            return obj;
+        }
+    }
+    // Retrieve by ID
+    getById(id) {
+        return new this.Parse.Query(this.New())
+        .get(id)
+        .then(result => {
+            this.Parse.defineAttributes(result, this.fields);
+            this.data = result;
+            return Promise.resolve(result);
+        })
+        .catch(error => Promise.reject(error));
+    }
     // Retrieve all phenotypes
-    getAllPhenotypes() {        
-        return new this.Parse.Query(this.New())                               
-        .find(results => {                
-            results.forEach(result =>          
-                this.Parse.defineAttributes(result, this.fields) 
-                 
-                );                
-            this.data = results;   
+    getAllPhenotypes() {
+        return new this.Parse.Query(this.New())
+        .find(results => {
+            results.forEach(result =>
+                this.Parse.defineAttributes(result, this.fields)
 
-            return Promise.resolve(results);            
-            })            
-            .catch(error => Promise.reject(error));    
+                );
+            this.data = results;
+
+            return Promise.resolve(results);
+            })
+            .catch(error => Promise.reject(error));
         }
 }
 
 // Register the model
-angular    
-.module('components')    
+angular
+.module('components')
 .service('PhenotypeModel', PhenotypeModel);
